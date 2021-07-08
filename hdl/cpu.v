24192ba9e66b8abc009b0eee6863aafd5b8e8a46
Billy Hadlow <me@billyhadlow.com>
2021-07-08 21:05:14 +0100

    alu control module

--- hdl/cpu.v
+++ hdl/cpu.v
@@ -125,6 +125,35 @@ module alu(
 
 endmodule
 
+module alu_control(alu_control, alu_op, opcode);
+	
+	input [1:0] alu_op;
+	input [3:0] opcode;
+
+	output reg [2:0] alu_control;
+
+	wire [5:0] alu_control_in;
+
+	assign alu_control_in = {alu_op, opcode};
+
+	always @(alu_control_in)
+		casex(alu_control_in)
+			6`b10xxxx: alu_control = 3`b000;
+			6`b01xxxx: alu_control = 3`b001;
+			6`b000010: alu_control = 3`b000;
+			6`b000011: alu_control = 3`b001;
+			6`b000100: alu_control = 3`b010;
+			6`b000101: alu_control = 3`b011;
+			6`b000110: alu_control = 3`b100;
+			6`b000111: alu_control = 3`b101;
+			6`b001000: alu_control = 3`b110;
+			6`b001001: alu_control = 3`b111;
+			default: alu_control = 3`b000;
+		endcase
+	end
+
+endmodule
+
 module cpu #(
   parameter integer DATA_WIDTH = 8
 ) (
